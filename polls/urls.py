20e817f23bd3e7fa071f604addd1aa1183d3c5cc
l-duan <christianbadguy@123.com>
2018-03-22 20:26:14 +0800

    add polls templates

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,13 @@ __auth__ = 'christian'
 from django.urls import path, re_path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    re_path('^index/$', views.index, name='index'),
+    re_path('^$', views.index, name='index'),
+    # ex: /polls/5/
+    re_path('^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    re_path('^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    re_path('^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Choice, Question
 # Create your views here.
-
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # render()函数的第一个位置参数是请求对象（就是view函数的第一个参数），第二个位置参数是模板。还可以有一个可选的第三参数，一个字典，
+    # 包含需要传递给模板的数据。最后render函数返回一个经过字典数据渲染过的模板封装而成的HttpResponse对象。
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
