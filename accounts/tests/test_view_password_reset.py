03d5367e09eceb41c0218bb78097c9f37c918406
l-duan <christianbadguy@123.com>
2018-05-10 22:47:03 +0800

    add 5 files about signup and password

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -88,6 +88,7 @@ class PasswordResetDoneTests(TestCase):
         view = resolve('/accounts/reset/done/')
         self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
 
+
 class PasswordResetConfirmTests(TestCase):
     def setUp(self):
         user = User.objects.create_user(username='john', email='john@doe.com', password='123abcdef')
@@ -148,29 +149,6 @@ class InvalidPasswordResetConfirmTests(TestCase):
         self.assertContains(self.response, 'invalid password reset link')
         self.assertContains(self.response, 'href="{0}"'.format(password_reset_url))
 
-
-class InvalidPasswordResetConfirmTests(TestCase):
-    def setUp(self):
-        user = User.objects.create_user(username='john', email='john@doe.com', password='123abcdef')
-        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
-        token = default_token_generator.make_token(user)
-
-        '''
-        invalidate the token by changing the password
-        '''
-        user.set_password('abcdef123')
-        user.save()
-
-        url = reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
-        self.response = self.client.get(url)
-
-    def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
-
-    def test_html(self):
-        password_reset_url = reverse('password_reset')
-        self.assertContains(self.response, 'invalid password reset link')
-        self.assertContains(self.response, 'href="{0}"'.format(password_reset_url))
 
 class PasswordResetCompleteTests(TestCase):
     def setUp(self):
